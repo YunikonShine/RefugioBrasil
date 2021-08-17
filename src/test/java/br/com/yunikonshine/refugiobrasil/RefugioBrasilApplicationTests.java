168fcb291901d38c9f676302737cd48808d22355
Vinicius Moreira <vinicius.moraes@iteris.com.br>
2021-08-17 13:02:30 -0300

    first commit

---
+++ src/test/java/br/com/yunikonshine/refugiobrasil/RefugioBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yunikonshine.refugiobrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefugioBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
